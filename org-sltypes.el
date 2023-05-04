6351225e17175f22ff5ba9a1428dc507c4dba15e
Phil Dyer <phildyer@protonmail.com>
2023-05-04 15:42:01 +1000

    Add function that calls org-super-links with appropriate settings

--- org-sltypes.el
+++ org-sltypes.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: tools, hypermedia
 ;; Homepage: https://github.com/PhDyellow/org-sltypes
-;; Package-Requires: ((emacs "24.3") (org-super-links "0.4"))
+;; Package-Requires: ((emacs "24.3") (org-super-links "0.4") (cl-lib "0.5"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -50,12 +50,62 @@
 ;;; Code:
 
 (require 'org-super-links)
+(require 'subr)
+(require 'cl-lib)
 
 (defun org-sltypes-time-stamp-inactive ()
   "Return an 'org-mode' inactive date stamp."
   (format "%s "
 	  (format-time-string (org-time-stamp-format t t) (current-time)))
 
+  ;; function that takes toggles and list with link variables and does the right thing
+  (defun org-sltypes-link (inverse-backlinks-p
+			   inline-p
+			   use-stored-p
+			   backlink-draw
+                           link-draw
+                           back-pre
+                           link-pre
+                           back-post
+			   link-post)
+    "generalised org-super-links.
+
+`inverse-backlinks-p' is experimental, attempts to create a backlink TO current not FROM target node, usually the target node gets a backlink. Undefined behaviour if set with inline-p.
+
+`inline-p' insert forward link inline, not in drawer.
+
+`use-stored-p' use the target stored by org-super-links-store-link, if nil, request a target from user.
+
+BACKLINK-DRAW gives the name of the drawer at the link target.
+Nil to insert link under target heading.
+LINK-DRAW gives the name of the drawer at the link source.
+Nil to insert link at point.
+
+The rest of the parameters set prefix and postfix strings.
+If nil, nothing is inserted, if a string, the string is inserted.
+If a function, the function is called without arguments and
+must return a string.
+BACK-PRE
+LINK-PRE
+BACK-POST
+LINK-POST"
+    (interactive)
+    (when inverse-backlinks-p
+      (cl-rotatef link-draw backlink-draw)
+      (cl-rotatef back-pre link-pre)
+      (cl-rotatef back-post link-post))
+    (when inline-p
+      (setq link-pre (replace-regexp-in-string "\\(_\\|-\\)" " " (downcase link-draw)) ;; may set to nil
+	    (setq link-draw nil))
+      (let ((link-command (if use-stored-p #'org-super-links-insert-link #'org-super-links-link))
+	    (org-super-links-backlink-into-drawer (if backlink-draw backlink-draw org-super-links-backlink-into-drawer))
+            (org-super-links-related-into-drawer (if link-draw link-draw org-super-links-related-into-drawer))
+            (org-super-links-backlink-prefix (if back-pre back-pre org-super-links-backlink-prefix))
+            (org-super-links-backlink-postfix (if back-post back-post org-super-links-backlink-postfix))
+            (org-super-links-link-prefix (if link-pre link-pre org-super-links-link-prefix))
+            (org-super-links-link-postfix (if link-post link-post org-super-links-link-postfix)))
+	(funcall link-command)))
+    
 
 (defmacro org-sltypes-make-insert (&optional
                                    backlink-draw
